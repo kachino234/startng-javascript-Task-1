19df15f2547094d6c2cff5fdf868538523dfdac3
kachino234 <agonsi.onyedikachi@gmail.com>
2020-03-22 13:19:29 +0100

    Create script.js
    Javascript first task start.ng internship

---
+++ script.js
@@ -0,0 +1,18 @@
+/*This block of code stores myname in a variable and courses
+taken on start.ng is stored in an array then it outputs both my name and courses to the console
+*/
+var myName = "Agonsi Onyedikachi David";
+var myCourses = ["HTMl", " CSS", " JavaScript", " UI/UX"];
+console.log("My name is: " + myName);
+console.log(" courses: " + myCourses);
+
+//console.log(myCourses.length);
+
+// A for loop code that displays even numbers from 1 to 200(inclusive) because the number of my courses is even
+
+for (var i=1; i<201; i++)
+{ if (i % 2 === 0){
+        console.log(i)
+    }
+    }
+
